32c07cf963bac17a797a009361e1a0c2d4c97eca
tom <tomcatleee@gmail.com>
2016-11-01 10:14:50 +0800

    修改checkin checkout逻辑

--- src/ext/caep/integration/util/IntegrationUtil.java
+++ src/ext/caep/integration/util/IntegrationUtil.java
@@ -496,8 +496,8 @@ public class IntegrationUtil implements RemoteAccess {
 		Workable result = null;
 		try {
 
-			if (WorkInProgressHelper.isWorkingCopy(workable)) {
-				result = workable;
+			if (WorkInProgressHelper.isCheckedOut(workable)) {
+				result = WorkInProgressHelper.service.workingCopyOf(workable);
 			} else {
 				CheckoutLink clink = WorkInProgressHelper.service.checkout(workable, WorkInProgressHelper.service.getCheckoutFolder(), "");
 				result = clink.getWorkingCopy();
@@ -510,6 +510,18 @@ public class IntegrationUtil implements RemoteAccess {
 			e.printStackTrace();
 		} catch (PersistenceException e) {
 			e.printStackTrace();
+		} catch (WTException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static boolean isCheckout(Workable workable) {
+		boolean result = false;
+		try {
+			if (WorkInProgressHelper.isCheckedOut(workable) || WorkInProgressHelper.isWorkingCopy(workable)) {
+				result = true;
+			}
 		} catch (WTException e) {
 			e.printStackTrace();
 		}
@@ -534,9 +546,26 @@ public class IntegrationUtil implements RemoteAccess {
 		return result;
 	}
 
+	public static Workable undoCheckout(Workable workable) {
+		Workable result = workable;
+		if (WorkInProgressHelper.isWorkingCopy(workable)) {
+			try {
+				result = WorkInProgressHelper.service.undoCheckout(workable);
+			} catch (WorkInProgressException e) {
+				e.printStackTrace();
+			} catch (WTPropertyVetoException e) {
+				e.printStackTrace();
+			} catch (PersistenceException e) {
+				e.printStackTrace();
+			} catch (WTException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
 	public static void updateName(WTDocument doc, String newName) {
 		if (newName != null && !newName.equals("") && !doc.getName().equals(newName)) {
-			doc = (WTDocument) IntegrationUtil.checkout(doc);
 			WTDocumentMaster master = (WTDocumentMaster) doc.getMaster();
 			WTDocumentMasterIdentity docMasterIdentity;
 			try {
@@ -553,7 +582,6 @@ public class IntegrationUtil implements RemoteAccess {
 
 	public static void updateName(WTPart part, String newName) {
 		if (newName != null && !newName.equals("") && !part.getName().equals(newName)) {
-			part = (WTPart) IntegrationUtil.checkout(part);
 			WTPartMaster master = (WTPartMaster) part.getMaster();
 			WTPartMasterIdentity partMasterIdentity;
 			try {
